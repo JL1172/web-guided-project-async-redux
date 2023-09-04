1becd7309a19cd71db8d69c539c5a9e3c04bbd62
Jacob Lang <jacoblang72@comcast.net>
2023-09-04 15:10:13 -0400

    firstM

--- middleware-example/src/index.js
+++ middleware-example/src/index.js
@@ -10,7 +10,7 @@ import FriendsList from './components/FriendsList';
 import './styles.css';
 
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 function App() {
   return (
